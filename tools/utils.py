230d197d61876d7c0d734fd3931a8e766cb0e075
logan0116 <1809361150@qq.com>
2024-08-09 22:45:05 +0800

    fix bug: utils

---
+++ tools/utils.py
@@ -0,0 +1,35 @@
+import time
+from zhipuai import ZhipuAI
+import configparser
+import requests
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+def chat(each_prompt: list, local_mode=False):
+    """
+    example
+    :param each_prompt:
+    :param local_mode:
+    :return:
+    """
+    if not local_mode:
+        client = ZhipuAI(api_key=config['zhipu_chat']['api_key'])
+        response = client.chat.completions.create(
+            model="glm-4-air",
+            messages=each_prompt
+        )
+        return response.choices[0].message.content
+    else:
+        # local mode
+        port = config['local_chat']['port']
+        url = f"http://localhost:{port}/chat"
+        res = requests.post(
+            url,
+            json={
+                "inputs": each_prompt[-1]['content'],
+                "history": [{"role": 'system', "content": '你是一个优秀的助手。'}] + each_prompt[:-1]
+            },
+            timeout=60)
+        return res.json()['data']
